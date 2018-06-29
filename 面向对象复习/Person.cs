9c5940bac3c3a631cb1b831c8ce3b5f2d7404ad8
扬 童 <452949529@qq.com>
2018-06-29 14:48:30 +0800

    静态方法与属性

---
+++ 面向对象复习/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象复习
+{
+    class Person
+    {
+        string _name;
+        char _gaden;
+        int _age;
+
+        public string Name
+        {
+            set
+            {
+                _name = value;
+            }
+            get
+            {
+                if (_name == "童扬")
+                {
+                    _name += "徐娅";
+                }
+                return _name;
+            }
+        }
+
+        public static Person PersonWithName(string name)
+        {
+            Person person = new Person();
+            person.Name = name;
+            return person;
+        }
+    }
+}
---
+++ 面向对象复习/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象复习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "童扬";
+            Person p1 = Person.PersonWithName("徐娅");
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p1.Name);
+            Console.ReadKey();
+        }
+    }
+}
