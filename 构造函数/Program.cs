6ff3bed760912e0dd31b00dac25f58c96918926d
扬 童 <452949529@qq.com>
2018-07-11 13:11:48 +0800

    构造函数

---
+++ 构造函数/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 构造函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("tongyang", 30, "男");
+        }
+    }
+}
---
+++ 构造函数/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 构造函数
+{
+    class Student
+    {
+        private string _name;
+        private int _age;
+        private string _gender;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+        public string Gender { get => _gender; set => _gender = value; }
+
+        public Student(string name, int age, string gender)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Gender = gender;
+        }
+    }
+}
